00a5b87e17edb794ccb2eec801037cf435ee0ac0
Kartik Jain <kartikjainmrt@gmail.com>
2022-05-25 16:58:21 +0530

    Initialized Server

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hi');
+})
+
+const PORT = process.env.PORT || 3000;      //if not specified port is 3000
+
+app.listen(PORT,()=>{
+    console.log(`Server Listning on Port ${PORT}`);
+});
