4199c71c46b2515729c9d62b4f601ea9c687ee87
Dipan Das <dipanisboss@gmail.com>
2022-05-29 15:36:57 +0530

    Minor changes

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const BookSchema = new mongoose.Schema({
+const bookSchema = new Schema({
     isbn :{
         type : String,
         required : true
@@ -31,4 +32,4 @@ const BookSchema = new mongoose.Schema({
     },
 }, { timestamps: true });
 
-module.exports = mongoose.model("Book",BookSchema);
+module.exports = mongoose.model("Book",bookSchema);
