9a5c561830691ff379f1e51a6f2836325eae2717
Dipan Das <dipanisboss@gmail.com>
2022-05-29 15:36:01 +0530

    Removed error

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const routes = require('./routes');
 const app = express();
 require('./db');
 
-app.get('/',(req,res)=>{
-    res.send('Hi');
-})
-
 app.use("/", routes);
 
 const PORT = process.env.PORT || 3000;      //if not specified port is 3000
